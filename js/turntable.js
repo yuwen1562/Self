91b7148c1cbb84d28a1f48fbc351788370cd74be
yuwen <59990662+yuwen1562@users.noreply.github.com>
2022-01-06 22:17:06 +0800

    0106-1016

--- js/turntable.js
+++ js/turntable.js
@@ -1,38 +1,39 @@
-let go_btn = document.getElementById('go_btn')
-let reset_btn = document.getElementById('reset_btn')
+let go_btn = document.getElementById("go_btn");
+let reset_btn = document.getElementById("reset_btn");
 
-let deg = 0 //轉圈後停留的度數
-let offOn = true //是否正在抽獎
+let deg = 0; //轉圈後停留的度數
+let offOn = true; //是否正在抽獎
 
-reset_btn.onclick = function () {
-  debugger
-  go_btn.style.transition = 'none'
-  go_btn.style.transform = 'rotate(0deg)'
+reset_btn.onclick = function() {
+    debugger;
+    go_btn.style.transition = "none";
+    go_btn.style.transform = "rotate(0deg)";
 }
 
-go_btn.onclick = function () {
-  debugger
-  if (go_btn.style.transition != '') {
-    go_btn.style.transition = ''
-  }
-  offOn = !offOn
-  rotating()
+go_btn.onclick = function() {
+    debugger;
+    if (go_btn.style.transition != "") {
+        go_btn.style.transition = "";
+    }
+    offOn = !offOn;
+    rotating();
 }
+
 
 function rotating() {
-  let timer = null
-  let rdm = 0 //隨機度數
-  clearInterval(timer)
-  timer = setInterval(function () {
-    if (Math.floor(rdm / 360) < 4) {
-      rdm = Math.floor(Math.random() * 3600)
-    } else {
-      go_btn.style.transform = 'rotate(' + rdm + 'deg)'
-      clearInterval(timer)
-      setTimeout(function () {
-        offOn = !offOn
-        deg = rdm % 360
-      }, 4000)
-    }
-  }, 30)
+    let timer = null;
+    let rdm = 0; //隨機度數
+    clearInterval(timer);
+    timer = setInterval(function() {
+        if (Math.floor(rdm / 360) < 4) {
+            rdm = Math.floor(Math.random() * 3600);
+        } else {
+            go_btn.style.transform = "rotate(" + rdm + "deg)";
+            clearInterval(timer);
+            setTimeout(function() {
+                offOn = !offOn;
+                deg = rdm % 360;
+            }, 4000);
+        }
+    }, 30)
 }
